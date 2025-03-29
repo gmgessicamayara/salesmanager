069f1b41c7e4eb1a3dd632905b44c97c5ea73581
GESSICA MENDONCA <gm.gessica@outlook.com>
2025-03-28 22:23:09 -0300

    Product table list

--- vendas-app/src/components/products/recordList/index.tsx
+++ vendas-app/src/components/products/recordList/index.tsx
@@ -1,15 +1,29 @@
 import { Layout } from 'components/layout';
 import Link from 'next/link';
 import { ProductsTable } from './table';
+import { Product } from 'app/models/products';
+import useSWR from 'swr';
+import { httpClient } from 'app/http';
+import { AxiosResponse } from 'axios';
 
 export const ProductList: React.FC = () => {
+  const { data: result, error } = useSWR<AxiosResponse<Product[]>>(
+    '/api/products',
+    (url) => httpClient.get(url)
+  );
+
+  if (!result) {
+    return <div>Carregando</div>;
+  }
+
   return (
     <Layout titulo='Products'>
       <Link href='/catalogs/products'>
         <button className='button is-link'>New</button>
+        <br />
       </Link>
       <br />
-      <ProductsTable />
+      <ProductsTable products={result.data} />
     </Layout>
   );
 };
--- vendas-app/src/components/products/recordList/table/index.tsx
+++ vendas-app/src/components/products/recordList/table/index.tsx
@@ -1,24 +1,50 @@
-export const ProductsTable: React.FC = () => {
+import { Product } from 'app/models/products';
+
+interface TableProductsProps {
+  products: Array<Product>;
+}
+
+export const ProductsTable: React.FC<TableProductsProps> = ({ products }) => {
   return (
-    <table className='table'>
+    <table className='table is-hoverable'>
       <thead>
-        <th>ID</th>
-        <th>SKU</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Description</th>
-        <th>Actions</th>
+        <tr>
+          <th>ID</th>
+          <th>SKU</th>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Description</th>
+          <th>Registration date</th>
+          <th>Actions</th>
+        </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>ID</td>
-          <td>SKU</td>
-          <td>Name</td>
-          <td>Price</td>
-          <td>Description</td>
-          <td>Delete</td>
-        </tr>
+        {products.map((product) => (
+          <ProductRow key={product.id} product={product} />
+        ))}
       </tbody>
     </table>
+  );
+};
+
+interface ProducttRowProps {
+  product: Product;
+}
+
+const ProductRow: React.FC<ProducttRowProps> = ({ product }) => {
+  return (
+    <tr>
+      <td>{product.id}</td>
+      <td>{product.sku}</td>
+      <td>{product.name}</td>
+      <td>{product.price}</td>
+      <td>{product.description}</td>
+      <td>{product.registrationDate}</td>
+      <td>
+        <button className='button is-success'>Edit</button>
+
+        <button className='button is-danger'>Delete</button>
+      </td>
+    </tr>
   );
 };
